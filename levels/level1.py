21b13b4469d0c3c756faa80e5f50ecf3de764b7d
murphy <murphy@sissybounce.(none)>
2012-11-11 12:24:28 -0500

    added logic checks

--- levels/level1.py
+++ levels/level1.py
@@ -7,10 +7,24 @@ class TimingLevel(Level.Level):
 					 getBackgroundInstrument,getInstrumentList,isValidState,\
 					 isButtonPressValid,playBackgroundInstrument,update
 	
+
 	def __init__(self):
-		pass
+		self.numberOfBeats = 4
+		self.currentBeat = 1
 	def getEndText(self):
 		print "Level 1 end text"
+	def update(self):
+		metronome.upate()
+		if(metronome.isAtNextBeat()):
+				currentBeat = currentBeat + 1
+				print "Next beat - now on beat ", currentBeat
+	
+	def isComplete(self):
+		if (currentBeat > numberOfBeats):			
+			return True 
+
+	def isValidState(self):
+		return True
 
 
 if __name__ == '__main__':
