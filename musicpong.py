dbba059bff69be0b961ce7e7b14c34d2c5e1dfd3
murphy <murphy@sissybounce.(none)>
2012-11-11 14:06:58 -0500

    added midi keyboard checks

--- musicpong.py
+++ musicpong.py
@@ -1,100 +1,61 @@
 import pygame
+import pygame.midi
 import os
+import sc
 from levels.level1 import TimingLevel
 from pygame.locals import *
 
-NOTE = 0
-ACTION = 1
-		
 class Game:
 	def __init__(self):
-		self.screen = pygame.display.set_mode((640, 480))
-		pygame.display.set_caption('RockPong')
-
-		self.background = pygame.Surface(self.screen.get_size())
-		self.background = self.background.convert()
-		self.background.fill((0, 0, 0))
-		
-		# Blit everything to the screen
-		self.screen.blit(self.background, (0, 0))
-		pygame.display.flip()
-
-		# Initialise clock
-		self.clock = pygame.time.Clock()
-		
-		self.running = True
+		self.s_keyDown=144
+		self.s_velocityValue=127.0
+    		self.keyboards = (pygame.midi.Input(3),pygame.midi.Input(5))
+    		self.clock = pygame.time.Clock()
+    		self.running = True
 		
 		self.levels = [TimingLevel(),TimingLevel()]
 		self.level = self.levels.pop()
-		
-		self.guideLines(10,50,1,70,4,4) #todo - move this
+		self.level.getStartText()
 	
-	def guideLines(self,x,y,width,height,measures,beats):
-		self.beats = beats
-		count = beats*measures
-		self.gap = (640 - (width*(count+1)) - 20)/count
 		
-		for r in range(0,count+1):
-			if (r%beats == 0):
-				pygame.draw.rect(self.screen, (0,0,255), (x,y,width,height))
-			else:
-				pygame.draw.rect(self.screen, (0,255,0), (x,y,width,height/2))
-			x = x + self.gap
-
-	def handleKeyboardInput(self):
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				self.running = False
-				return
-			elif event.type == KEYDOWN:
-				if event.key == K_ESCAPE:
-					self.running = False
-				if event.key == K_TAB:
-					self.notePlayed = True;
-					self.previousNote = "note"
-			
-	def handleMusicInput(self):
-		if (self.ActivePlayer == source):	
-			if (self.state == NOTE):			
-				self.notePlayed = True;
-				if (self.playedGoodNote()):
-					#play success msg
-					pass
-				if (firstTurn):
-					self.firstTurn = False
-					pass
-				else: 
-					#play bad note
-					pass
-			else: 
-				self.action = action
-	
 	def update(self):
 		self.level.update()
+		event_get = pygame.fastevent.get
+		events = event_get()
+	
+		for keyboard in self.keyboards:
+			if keyboard.poll():
+				midi_events = keyboard.read(10)
+				level.handleMusicInput(self.getPlayer(keyboard),midi_events[0][0])
+
 		
 		if( self.level.isComplete() ):
-			print "VICTORY"
+			self.level.getEndText()
 			if (self.levels):
 				self.level = self.levels.pop()
-				self.level.start()
+				self.level.getStartText()
 			else:
+				print "All levels completed successfully!"
 				exit()
 	
+	def getPlayer(self,keyboardID):
+		if(keyboardID == self.keyboards[0]):
+				return 1
+		else:
+				return 2
+	
 def main():
+	
 	pygame.init()
+	pygame.fastevent.init()
+	pygame.midi.init()
 
 	game = Game() 
 
 	while game.running:
-	# Make sure game doesn't run at more than 60 frames per second
+		
 		game.clock.tick(60)
-		
-		game.handleKeyboardInput()
-		#game.handleMusicInput()
-		
 		game.update()
-		
-		pygame.display.update()
 
 if __name__ == '__main__': main()
 
