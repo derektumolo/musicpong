590a7fb93c4d9feac02939e1728ca64ccd343d38
murphy <murphy@sissybounce.(none)>
2012-11-11 12:12:59 -0500

    Added at next measure method

--- metronome.py
+++ metronome.py
@@ -1,18 +1,22 @@
 class Metronome:
+	
+	ticksPerMinute = 3600
+		
 	def __init__(self, bpm, timeSig, screen):
 		self.beat = 1
-		self.ticksPerBeat = bpm
+		self.bpm = bpm
+		self.tickerPerBeat = ticksPerMinute/bpm
 		self.timeSig = timeSig
 		self.screen = screen
+		self.tick = 0
 	
-	def circle(self,tick,gap):
-		x = tick*gap*self.ticksPerBeat/60/60
-		pygame.draw.circle(self.screen, (255,0,0), (x + 10,70),5)
-	
-	def update(self, tick):
-		self.circle()
+	def update(self):
+		tick = tick + 1 
+
+	def	atNextMeasure():
 		if (tick > self.ticksPerBeat * self.beat):
 			self.beat = self.beat + 1
 			if (self.beat%self.timeSig == 1):
 				return True
 		return False
+
